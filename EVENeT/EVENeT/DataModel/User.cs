9ea1ee890c86174647e735fd3145bd60df60e6d7
lap089 <tqlap@apcs.vn>
2015-12-19 16:49:22 +0700

    Add ImageAvatar to User class

--- EVENeT/EVENeT/DataModel/User.cs
+++ EVENeT/EVENeT/DataModel/User.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace EVENeT.DataModel
 {
   public class User
     {
         public string Username { get; set; }
-        public string Avatar { get; set; }
+        string avatar;
+        public string Avatar {
+            get {
+                return avatar;
+                }
+            set { avatar = value;
+                changeBitmap();
+                }
+        }
         public string Name { get; set; }
+
+        public BitmapImage ImageAvatar { get; set; } 
+
 
         public User(string Username, string Avatar, string Name)
         {
             this.Username = Username;
             this.Avatar = Avatar;
             this.Name = Name;
+        }
+
+        public async void changeBitmap()
+        {
+           StorageFile file = await StorageFile.GetFileFromPathAsync(avatar);
+           ImageAvatar = new BitmapImage();
+            await ImageAvatar.SetSourceAsync(await file.OpenAsync(FileAccessMode.Read));
         }
     }
 }
